f967a4ddd26c27f306d8d144f8d5e08e2a8bb097
Rustam Guseynov <guseynov.r.v@gmail.com>
2024-02-17 15:07:42 +0000

    Finish database optimization

--- blango/settings.py
+++ blango/settings.py
@@ -53,10 +53,12 @@ class Dev(Configuration):
         'django.contrib.staticfiles',
         'blog',
         'crispy_forms',
-        'crispy_bootstrap5'
+        'crispy_bootstrap5',
+        'debug_toolbar',
     ]
 
     MIDDLEWARE = [
+        "debug_toolbar.middleware.DebugToolbarMiddleware",
         'django.middleware.security.SecurityMiddleware',
         'django.contrib.sessions.middleware.SessionMiddleware',
         'django.middleware.common.CommonMiddleware',
@@ -65,6 +67,8 @@ class Dev(Configuration):
         'django.contrib.messages.middleware.MessageMiddleware',
         # 'django.middleware.clickjacking.XFrameOptionsMiddleware',
     ]
+
+    INTERNAL_IPS = ["192.168.10.93"]
 
     ROOT_URLCONF = 'blango.urls'
 
