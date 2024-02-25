57df38884d253747c70fe9dc1b189a7c6c0bd822
Rustam Guseynov <guseynov.r.v@gmail.com>
2024-02-25 08:22:21 +0000

    Finish django Allauth

--- blango/settings.py
+++ blango/settings.py
@@ -21,6 +21,12 @@ class Dev(Configuration):
     AUTH_USER_MODEL = "blango_auth.User"
     EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
     ACCOUNT_ACTIVATION_DAYS = 7
+    SITE_ID = 1
+    ACCOUNT_USER_MODEL_USERNAME_FIELD = None
+    ACCOUNT_EMAIL_REQUIRED = True
+    ACCOUNT_USERNAME_REQUIRED = False
+    ACCOUNT_AUTHENTICATION_METHOD = "email"
+    ACCOUNT_DEFAULT_HTTP_PROTOCOL='https'
 
     # Quick-start development settings - unsuitable for production
     # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -52,11 +58,16 @@ class Dev(Configuration):
         'django.contrib.contenttypes',
         'django.contrib.sessions',
         'django.contrib.messages',
+        'django.contrib.sites',
         'django.contrib.staticfiles',
         'blango_auth',
         'blog',
         'crispy_forms',
         'crispy_bootstrap5',
+        'allauth',
+        'allauth.account',
+        'allauth.socialaccount',
+        'allauth.socialaccount.providers.google',
         # 'debug_toolbar',
     ]
 
