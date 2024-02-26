fb2d791956a981cccd6b31b31aaef2ded6899a5d
Rustam Guseynov <guseynov.r.v@gmail.com>
2024-02-26 06:41:25 +0000

    Finish Serializers

--- blango/settings.py
+++ blango/settings.py
@@ -68,6 +68,7 @@ class Dev(Configuration):
         'allauth.account',
         'allauth.socialaccount',
         'allauth.socialaccount.providers.google',
+        'rest_framework'
         # 'debug_toolbar',
     ]
 
