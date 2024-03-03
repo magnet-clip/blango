f3b6a666f725cb006e32301350a9527caf2f9870
Rustam Guseynov <guseynov.r.v@gmail.com>
2024-03-03 06:28:29 +0000

    Finish simplejwt

--- blango/settings.py
+++ blango/settings.py
@@ -14,6 +14,7 @@ from pathlib import Path
 import os
 from configurations import Configuration, values
 import dj_database_url
+from datetime import timedelta
 
 class Dev(Configuration):
     # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -54,6 +55,7 @@ class Dev(Configuration):
             "rest_framework.authentication.BasicAuthentication",
             "rest_framework.authentication.SessionAuthentication",
             "rest_framework.authentication.TokenAuthentication",
+            "rest_framework_simplejwt.authentication.JWTAuthentication"
         ],
         "DEFAULT_PERMISSION_CLASSES": [
             "rest_framework.permissions.IsAuthenticatedOrReadOnly",
@@ -135,6 +137,11 @@ class Dev(Configuration):
             },
         },
     ]
+
+    SIMPLE_JWT = {
+        "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+        "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    }
 
     WSGI_APPLICATION = 'blango.wsgi.application'
 
